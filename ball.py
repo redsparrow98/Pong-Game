d332d43dbb192ba80bb59f38ea22c82118bb26e8
Natalija Prosic <162211449+redsparrow98@users.noreply.github.com>
2024-03-12 17:47:53 +0000

    Add files via upload

---
+++ ball.py
@@ -0,0 +1,38 @@
+from turtle import Turtle
+
+MOVE_FORWARD = 1
+
+class Ball(Turtle):
+
+  def __init__(self):
+    """Createse a square ball for the game"""
+    super().__init__()
+    self.penup()
+    self.shape("square")
+    self.color("white")
+    self.shapesize(stretch_wid= 1, stretch_len= 1)
+    self.x_move = 10
+    self.y_move = 10
+    self.move_speed = 0.1
+
+
+  def move(self):
+    """makes the ball move in continuous x and y cordinates"""
+    new_x = self.xcor() + self.x_move
+    new_y = self.ycor() + self.y_move
+    self.goto(new_x, new_y)
+
+  def bounce_y(self):
+    """reverses the y_move to -10 and it should bounce of the top an bottom wall"""
+    self.y_move *= -1
+
+  def bounce_x(self):
+    """reverses the x_move to -10 and it should bounce of the top an bottom wall"""
+    self.x_move *= -1
+    self.move_speed *= 0.9
+
+  def reset_position(self):
+    """reset ball to home and start in the opposite direction"""
+    self.goto(0, 0)
+    self.move_speed = 0.1
+    self.bounce_x()
---
+++ main.py
@@ -0,0 +1,62 @@
+#set up the screen with the middle separation (from turtle screen class)
+#create the boards class for users inputs (from turtle class inheritance)
+#create the boards class 2 users
+#create the pong (ball) class and make it move (from turtle inheritance)
+#figure out colision with the wall and bounce
+#detect colision with board and bounce back
+#detect missed ball
+#create the score class (also turtle inheritance)
+
+###########################################################################
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+from scoreboard import Scoreboard
+
+r_paddle = Paddle((380, 0))
+l_paddle = Paddle((-380, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen = Screen()
+screen.setup(width= 800, height= 600)
+screen.bgcolor("black")
+screen.title("Nats Pong Game")
+screen.tracer(0)
+
+
+screen.listen()
+screen.onkey(fun= r_paddle.go_up, key="Up")
+screen.onkey(fun= r_paddle.go_down, key="Down")
+
+screen.onkey(fun= l_paddle.go_up, key="w")
+screen.onkey(fun= l_paddle.go_down, key="s")
+
+game_is_on = True
+while game_is_on:
+  time.sleep(ball.move_speed)
+  screen.update()  
+  ball.move()
+
+  #detect colision with top and bottom wall to bounce
+  if ball.ycor() > 280 or ball.ycor() < -270:
+    ball.bounce_y()
+
+  #detect colision with paddles
+  if ball.distance(r_paddle) < 50 and ball.xcor() > 350 or ball.distance(l_paddle) < 50 and ball.xcor() < - 350:
+    ball.bounce_x()
+
+  #detect the ball going out of bounds and missing the r_paddle
+  if ball.distance(r_paddle) > 50 and ball.xcor() > 380:
+    ball.reset_position()
+    scoreboard.l_point()
+
+  #detect the ball going out of bounds and missing the l_paddle
+  if ball.distance(l_paddle) > 50 and ball.xcor() < -380:
+    ball.reset_position()
+    scoreboard.r_point()
+
+
+screen.exitonclick()
